8a2c9f4b7698450e5117fe09ce4229f9d282a117
Christopher Adams <christopher.r.adams@gmail.com>
2020-06-09 10:01:21 -0400

    Added script I use to generate index.txt

---
+++ scripts/update_indexes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -Eeuxo
+# Creates an index file, ``index.txt`` for a series useful directories.
+# This is just a recursive list of files inside a directory.
+paths=("$@")
+for path in "${paths[@]}"
+do
+    cd $path && find . > index.txt 2> /dev/null
+done
