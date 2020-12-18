729ae922fa594d1643746ed81c9a16644a912f1e
Christopher Adams <christopher.r.adams@gmail.com>
2020-12-18 16:42:13 -0500

    Initial draft of script to install dotfiles

---
+++ install_dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -Eeux -o pipefail
+dotfiles=(.vimrc .bash_login .psqlrc .gitconfig .gitignore_global)
+for dotfile in "${dotfiles[@]}"
+do
+    srcpath="$PWD/$dotfile"
+    destpath=~/$dotfile
+    if [[ ! -L "$destpath" ]]
+    then
+        ln -s $srcpath $destpath
+    fi
+done
