da1c0e681ef364e0254d660c25784b5d8c97134a
Christopher Adams <christopher.r.adams@gmail.com>
2020-12-19 11:48:01 -0500

    Install update_indexes script

--- install_scripts.sh
+++ install_scripts.sh
@@ -2,10 +2,10 @@
 set -Eeux -o pipefail
 SCRIPTS_BASE=~/scripts
 mkdir -p $SCRIPTS_BASE
-scripts=(f)
+scripts=(f update_indexes.sh)
 for script in "${scripts[@]}"
 do
-    srcpath="$PWD/$script"
+    srcpath="$PWD/scripts/$script"
     destpath=$SCRIPTS_BASE/$script
     if [[ ! -L "$destpath" ]]
     then
