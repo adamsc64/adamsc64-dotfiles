2dda58a928ac7438db31e097534dc845b912c095
Christopher Adams <christopher.r.adams@gmail.com>
2023-01-21 16:29:35 -0500

    Allowed random_numbers to remember last input

--- scripts/random_numbers.py
+++ scripts/random_numbers.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import random
 
+last_answer = None
+
 while True:
-    answer = raw_input('&> ')
+    answer = input("&> ")
+    if answer == "" and last_answer:
+        answer = last_answer
     try:
-        print random.randint(1, int(answer))
+        print(random.randint(1, int(answer)))
+        last_answer = answer
     except ValueError:
         pass
-
