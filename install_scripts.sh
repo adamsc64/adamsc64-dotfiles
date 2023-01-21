7faac1904d88c50f46b975683d3b8664f62ca977
Christopher Adams <christopher.r.adams@gmail.com>
2023-01-21 16:30:50 -0500

    Installed scripts with single line defs

--- install_scripts.sh
+++ install_scripts.sh
@@ -2,7 +2,15 @@
 set -Eeu -o pipefail
 SCRIPTS_BASE=~/scripts
 mkdir -p $SCRIPTS_BASE
-scripts=(f update_indexes.sh cdd trim random random_numbers.py img2say)
+scripts=(
+    f
+    update_indexes.sh
+    cdd
+    trim
+    random
+    random_numbers.py
+    img2say
+)
 for script in "${scripts[@]}"
 do
     srcpath="$PWD/scripts/$script"
