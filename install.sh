b55a72277ff11a2117dc678094b4bb8406bc61e5
Christopher Adams <christopher.r.adams@gmail.com>
2023-07-10 17:52:54 -0500

    Added install.sh for codespaces

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Run anything in this directory that starts with install_*.sh
+for file in install_*.sh; do
+  if [ -x "$file" ]; then
+    echo "Running $file"
+    ./"$file"
+  else
+    echo "Skipping $file (not executable)"
+  fi
+done
