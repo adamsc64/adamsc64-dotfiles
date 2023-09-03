84ef0bb730730c82b41183b6e0071d85b9945dc5
Christopher Adams <christopher.r.adams@gmail.com>
2023-09-03 11:50:19 -0400

    Sort the generated index files by length of line, to be able to find the shorted first

--- scripts/update_indexes.sh
+++ scripts/update_indexes.sh
@@ -5,5 +5,9 @@ set -Eeu -o pipefail
 paths=("$@")
 for path in "${paths[@]}"
 do
-    cd $path && find . > index.txt 2> /dev/null
+    TEMP_FILE="$(mktemp)"
+    cd $path && find . > ${TEMP_FILE} 2> /dev/null
+    awk '{ print length, $0 }' "${TEMP_FILE}" | sort -n | awk '{$1=""; print substr($0, 2)}' > "${TEMP_FILE}_sorted"
+    mv "${TEMP_FILE}_sorted" "$path/index.txt"
+    rm "${TEMP_FILE}"
 done
