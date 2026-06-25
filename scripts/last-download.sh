287187c581b13a16d9fcd9a33b0e16895f276cd1
Christopher Adams <christopher.r.adams@gmail.com>
2026-06-25 11:38:00 +0100

    scripts: allow last-download to filter by optional grep pattern

--- scripts/last-download.sh
+++ scripts/last-download.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-realpath "$(ls -1t "$HOME/Downloads"/* | head -n1)"
+# Usage: last-download.sh [grep_pattern]
+if [[ -n "$1" ]]; then
+	realpath "$(ls -1t "$HOME/Downloads"/* | grep -- "$1" | head -n1)"
+else
+	realpath "$(ls -1t "$HOME/Downloads"/* | head -n1)"
+fi
